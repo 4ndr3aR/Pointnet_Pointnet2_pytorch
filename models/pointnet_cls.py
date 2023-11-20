7fff4ef1faaf62f2c46710ed76d6d4742131bef0
Andrea Ranieri <andrea.ranieri@cnr.it>
2023-11-20 14:48:52 +0100

    Added a first attempt of regression network to pointnet_cls.py (that now shouldn't have the `_cls` suffix at all, but, hey ¯\_(ツ)_/¯

--- models/pointnet_cls.py
+++ models/pointnet_cls.py
@@ -3,8 +3,31 @@ import torch.utils.data
 import torch.nn.functional as F
 from pointnet_utils import PointNetEncoder, feature_transform_reguliarzer
 
+class SigmoidRange(Module):
+    '''
+    Shape:
+        - Input: :math:`(*)`, where :math:`*` means any number of dimensions.
+        - Output: :math:`(*)`, same shape as the input.
+
+
+    Examples::
+
+        >>> m = nn.Sigmoid()
+        >>> input = torch.randn(2)
+        >>> output = m(input)
+    '''
+
+    def __init__(self, low, high):
+	super(Sigmoid, self).__init__()
+	self.low  = low
+	self.high = high
+
+    def forward(self, input: Tensor) -> Tensor:
+        return torch.sigmoid(input) * (self.high - self.low) + self.low 
+
+
 class get_model(nn.Module):
-    def __init__(self, k=40, normal_channel=True):
+    def __init__(self, out_features=40, normal_channel=True, y_range=None):
         super(get_model, self).__init__()
         if normal_channel:
             channel = 6
@@ -13,11 +36,14 @@ class get_model(nn.Module):
         self.feat = PointNetEncoder(global_feat=True, feature_transform=True, channel=channel)
         self.fc1 = nn.Linear(1024, 512)
         self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, k)
+        self.fc3 = nn.Linear(256, out_features)
         self.dropout = nn.Dropout(p=0.4)
         self.bn1 = nn.BatchNorm1d(512)
         self.bn2 = nn.BatchNorm1d(256)
-        self.relu = nn.ReLU()
+        if y_range is not None:
+            self.sigmoid_range = SigmoidRange(y_range[0], y_range[1])
+        else:
+            self.relu = nn.ReLU()
 
     def forward(self, x):
         x, trans, trans_feat = self.feat(x)
